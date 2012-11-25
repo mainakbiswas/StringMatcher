b32de59b92325391f487cf631493f279ceac1119
mainak <mainak@mainak-VPCEB44EN.(none)>
2012-11-25 22:27:23 +0530

    Fuzzy string matcher 1st commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fuzzywuzzy',
+    version='0.1',
+    description='Fuzzy string matching in python',
+    author='Mainak Biswas',
+    author_email='mainakbiswas@ymail.com',
+    url='https://github.com/mainakbiswas/stringmatcher',
+    packages=['fuzzywuzzy'])
